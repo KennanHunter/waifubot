caaf0d3690d47eac77ad5bb706649f7448ffb5b5
Kennan Hunter <kennanhunter5@gmail.com>
2022-12-23 04:12:56 -0500

    init

---
+++ commands/hello.ts
@@ -0,0 +1,14 @@
+import { CommandContext, SlashCommand, SlashCreator } from 'slash-create';
+
+export default class HelloCommand extends SlashCommand {
+  constructor(creator: SlashCreator) {
+    super(creator, {
+      name: 'hello',
+      description: 'Says hello to you.'
+    });
+  }
+
+  async run(ctx: CommandContext) {
+    return `Hello, ${ctx.member?.displayName ?? ctx.user.username}!`;
+  }
+}
