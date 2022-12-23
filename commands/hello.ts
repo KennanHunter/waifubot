7acfbf7f3eb10945b4c72b49c7375013e5d2a25c
Kennan Hunter <kennanhunter5@gmail.com>
2022-12-23 04:34:10 -0500

    support lint-staged and prettier

--- commands/hello.ts
+++ commands/hello.ts
@@ -1,14 +1,14 @@
-import { CommandContext, SlashCommand, SlashCreator } from 'slash-create';
+import { CommandContext, SlashCommand, SlashCreator } from "slash-create";
 
 export default class HelloCommand extends SlashCommand {
-  constructor(creator: SlashCreator) {
-    super(creator, {
-      name: 'hello',
-      description: 'Says hello to you.'
-    });
-  }
+	constructor(creator: SlashCreator) {
+		super(creator, {
+			name: "hello",
+			description: "Says hello to you.",
+		});
+	}
 
-  async run(ctx: CommandContext) {
-    return `Hello, ${ctx.member?.displayName ?? ctx.user.username}!`;
-  }
+	async run(ctx: CommandContext) {
+		return `Hello, ${ctx.member?.displayName ?? ctx.user.username}!`;
+	}
 }
