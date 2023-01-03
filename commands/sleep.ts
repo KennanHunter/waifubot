5886a913f5aa5417049619a238333f078447cf2e
Kennan Hunter <kennanhunter5@gmail.com>
2023-01-02 23:05:06 -0500

    simple user target command

---
+++ commands/sleep.ts
@@ -0,0 +1,33 @@
+import {
+	CommandContext,
+	CommandOptionType,
+	SlashCommand,
+	SlashCreator,
+	User,
+} from "slash-create";
+
+export default class SleepCommand extends SlashCommand {
+	constructor(creator: SlashCreator) {
+		super(creator, {
+			name: "sleep",
+			description: "Put someone to sleep",
+			options: [
+				{
+					type: CommandOptionType.USER,
+					name: "target",
+					description: "Who do you want to put to sleep?",
+				},
+			],
+		});
+	}
+
+	async run(ctx: CommandContext) {
+		const target: User = ctx.options.target;
+
+		if (!target) {
+			return "Specify a user to put to sleep";
+		}
+
+		return `${target.username}!`;
+	}
+}
